34cc9fbdfc723a4891a5d9f5e1be4d7ce7779e0e
AlexandrLagun <alexanderlagun717@gmail.com>
2019-11-24 23:22:24 +0300

    1a)final

--- 2.JS/1.ArrayProcessingTool/c)SelectionTask/index1.js
+++ 2.JS/1.ArrayProcessingTool/c)SelectionTask/index1.js
@@ -1,11 +1,11 @@
-function getMaxSubSum(arr){
+function getSeqMaxSum(arr){
             var sum = 0;
             var max = 0;
-            //  получаем все возможные суммы.
+            //  Получаем все возможные суммы элементов массива
             for(var i=0;i<arr.length;i++){
-                for(var p=i;p < arr.length;p++){
-                    sum += arr[p];
-                    // Проверяем максимальную сумму в данный проход цикла.
+                for(var j=i;j < arr.length;j++){
+                    sum += arr[j];
+                    // Проверяем максимальная ли сумма в данный момент
                     if(sum < 0){
                         continue;
                     }else if(sum > max){
@@ -16,5 +16,5 @@ function getMaxSubSum(arr){
             }
             return (max > 0)? max: -1;
         }
-        console.log(getMaxSubSum( [-1,1,2,3,4,5,-15,-39] ));
+        console.log(getSeqMaxSum( [1,2,3,-15,1,4,8] ));
        
