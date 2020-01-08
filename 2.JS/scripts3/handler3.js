dd9055acc0c1a8bf4bdcb5ef6e5c6ec301e09198
Alexandr Lahun <Aliaksandr.Lahun@itechart-group.com>
2020-01-08 13:19:33 +0300

    modified 1st task and the 3rd exercise of the 2nd task

--- 2.JS/scripts3/handler3.js
+++ 2.JS/scripts3/handler3.js
@@ -7,28 +7,44 @@ var formatButton = document.getElementById('format_sentence');
             var formatType  = document.getElementById('format_type').value;
            
             if (maxLength !== undefined && maxLength > 0) {
+
                 var elem1 = document.getElementById('alert_max_length');
                 if (str.length > maxLength) { 
-                    elem1.innerHTML =  'too big length of the string!' ; 
+                    elem1.innerHTML =  'Too big length of the string! String was shortened.' ; 
+                    str = str.slice(0, maxLength);
                 } else {
                      elem1.innerHTML = '';
                   }
+                
             } 
              
             var result = textHandler(str, maxLength, maxRows, formatType);
-           
+
+            if (maxRows !== undefined && maxRows > 0) {
+
+                var lines = result.split('<br/>');
+                var elem2 = document.getElementById('max_row');
+                if (lines.length > maxRows) { 
+                    elem2.innerHTML =  `Big number of rows! Only ${maxRows} lines will be shown.`; 
+                    var i = 0;
+                    result = "";
+
+                    while(i < maxRows) {
+                        result += lines[i] + "<br/>";
+                        i++; 
+                    }
+        
+
+                } else {
+                    elem2.innerHTML = '';
+                 }
+                
+            }
+
+
+
             document.getElementById('result4').innerHTML = `Result: <br/><br/>${result}`;
             if (str === "") {
                 document.getElementById('result4').innerHTML = `Please, enter a string`;
-            }
-
-            if (maxRows !== undefined && maxRows > 0) {
-                var lines = result.split('<br/>').length;
-                var elem2 = document.getElementById('max_row');
-                if (lines - 1 > maxRows) { 
-                    elem2.innerHTML = 'too much lines in the text!'; 
-                } else {
-                    elem2.innerHTML = '';
-                  }
             }
         });
