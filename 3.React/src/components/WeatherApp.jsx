d2d9df03d772875fe4f73039ec04475bdc0afa27
AlexandrLagun <alexanderlagun717@gmail.com>
2020-01-19 23:31:27 +0300

    react_project

---
+++ 3.React/src/components/WeatherApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+import ChangeLocation from './ChangeLocation';
+import Success from './Success';
+
+const WeatherApp = () => (
+  <>
+    <Header />
+    <ChangeLocation />
+    <Success />
+  </>
+);
+export default WeatherApp;
