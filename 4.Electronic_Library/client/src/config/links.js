7f51fd3d2fcd1ab29fad896b71236435cfc998d2
AlexandrLagun <alexanderlagun717@gmail.com>
2020-02-18 08:40:24 +0300

    links constantsUser

--- 4.Electronic_Library/client/src/config/links.js
+++ 4.Electronic_Library/client/src/config/links.js
@@ -2,25 +2,30 @@ export const links = {
  
   //общие ссылки
 
-
   SIGN_UP_PAGE: '/account/signup',
   SIGN_OUT_PAGE: '/account/signout',
   LOG_IN_PAGE: '/account/login',
-  
 
   MAIN_PAGE_PATH: '/',
-  USER_PAGE: '/account',
-  ABOUT_INFO_PAGE: '/about',
-
   BOOKS_PAGE: '/books',
   BOOK_PAGE: '/books/:id/info',
+  //USER_PAGE: '/account',
+  ABOUT_INFO_PAGE: '/about',
+
+  
 
   //закрытые ссылки
 
-   PROFILE_PAGE: '/account/profile', // ссылка на профиль админа
+  PROFILE_PAGE: '/account/profile', // ссылка на профиль зарегистрированного пользователя или админа
+  
+  // доступны только админу
 
-
-
+  HANDED_OUT_BOOKS_PAGE: '/books/handedout',
+  CREATE_AUTHOR_PAGE: '/authors/create',
+  CREATE_BOOK_PAGE: '/books/create',
+  BLOCK_USER_PAGE: '/users/blocking',
+  BLOCKED_USERS_PAGE: '/users/blocked',
+   
 
 
 
